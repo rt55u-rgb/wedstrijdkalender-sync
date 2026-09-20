590aedd2988cb0810efe39b75c236c041ca7e5ec
JvdLan <jeltlangenberg@gmail.com>
2026-09-20 22:11:50 +0200

    .

--- sync.py
+++ sync.py
@@ -1,11 +1,58 @@
 from parser import load_matches
 from calendar_sync import GoogleCalendar
 
+print("Wedstrijden laden...")
 matches = load_matches()
+
+print(f"{len(matches)} wedstrijden gevonden")
 
 calendar = GoogleCalendar()
 
-for match in matches:
-    calendar.add_match(match)
+print("Agenda ophalen...")
+event_index = calendar.build_index()
 
-print(f"\n{len(matches)} wedstrijden gesynchroniseerd.")
+excel_ids = set()
+
+added = 0
+updated = 0
+deleted = 0
+
+print("\nSynchroniseren...\n")
+
+for match in matches:
+
+    excel_ids.add(match.uid)
+
+    if match.uid in event_index:
+
+        calendar.update_match(
+            event_index[match.uid],
+            match
+        )
+
+        updated += 1
+
+    else:
+
+        calendar.add_match(match)
+
+        added += 1
+
+
+for uid, event in event_index.items():
+
+    if uid not in excel_ids:
+
+        calendar.delete_event(event)
+
+        deleted += 1
+
+
+print("\n--------------------------------")
+
+print(f"Toegevoegd : {added}")
+print(f"Bijgewerkt : {updated}")
+print(f"Verwijderd : {deleted}")
+
+print("--------------------------------")
+print("Synchronisatie voltooid.")
